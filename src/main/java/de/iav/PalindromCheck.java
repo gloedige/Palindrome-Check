f71a605fd32e982020e5779e83a5ebd63f03822e
Gerd Lödige <gerd.loedige@posteo.de>
2023-05-05 14:56:52 +0200

    added check for 2 consecutive and 3 consecutive letters

--- src/main/java/de/iav/PalindromCheck.java
+++ src/main/java/de/iav/PalindromCheck.java
@@ -2,8 +2,35 @@ package de.iav;
 
 public class PalindromCheck {
 
-    public static boolean isPalindrome(String stringToCheck) {
-        return true;
+    public static void main(String[] args) {
+        isPalindrome("a");
+        //just a comment
     }
+
+
+    public static boolean isPalindrome(String stringToCheck) {
+        if (stringToCheck.equals(" ")) {
+            System.out.println("String contains whitespace");
+            return true;
+        }
+        else if (stringToCheck.length() == 1) {
+            System.out.println("Length of string: " + stringToCheck.length());
+            return true;
+        }
+        else if (stringToCheck.length() == 2){
+            if (stringToCheck.charAt(0) == stringToCheck.charAt(1) ) {
+                System.out.println("String contains two consecutive letters");
+                return true; }
+             else if (stringToCheck.charAt(0) != stringToCheck.charAt(1) ) {
+                 System.out.println("String contains two different consecutive letters");
+                 return false;
+            }
+        }
+        else if (stringToCheck.charAt(0) == stringToCheck.charAt(1) && stringToCheck.charAt(1) == stringToCheck.charAt(2)) {
+            System.out.println("String contains three consecutive letters");
+            return true;
+        }
+        return false;
+        }
 
 }
--- src/test/java/de/iav/PalindromCheckTest.java
+++ src/test/java/de/iav/PalindromCheckTest.java
@@ -13,10 +13,44 @@ class PalindromCheckTest {
     void checkPalindorm_whenStringIsWhiteSpace_shouldReturnTrue() {
         //given
         String StringToCheck = " ";
-        boolean expected = true;
         //when
         boolean isPalindrome = PalindromCheck.isPalindrome(StringToCheck);
         //then
         assertTrue(isPalindrome);
     }
-}
+    @Test
+    void checkPalindorm_whenStringContainsOneLetter_shouldReturnTrue() {
+        //given
+        String StringToCheck = "a";
+        //when
+        boolean isPalindrome = PalindromCheck.isPalindrome(StringToCheck);
+        //then
+        assertTrue(isPalindrome);
+    }
+    @Test
+    void checkPalindorm_whenStringContainsConsecutiveLetters_shouldReturnTrue() {
+        //given
+        String StringToCheck = "AA";
+        //when
+        boolean isPalindrome = PalindromCheck.isPalindrome(StringToCheck);
+        //then
+        assertTrue(isPalindrome);
+    }
+    @Test
+    void checkPalindorm_whenStringContainsDifferentConsecutiveLetters_shouldReturnFalse() {
+        //given
+        String StringToCheck = "ab";
+        //when
+        boolean isPalindrome = PalindromCheck.isPalindrome(StringToCheck);
+        //then
+    }
+    @Test
+    void checkPalindorm_whenStringContainsThreeConsecutiveLetters_shouldReturnTrue() {
+        //given
+        String StringToCheck = "AAA";
+        //when
+        boolean isPalindrome = PalindromCheck.isPalindrome(StringToCheck);
+        //then
+    }
+
+    }
